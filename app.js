70b52bdc4c4f93802b313fa7847e99c91b5d6e66
IT24102379 <it24102379@my.sliit.lk>
2025-01-08 15:52:31 +0530

    Level-2 security

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption"); 
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema =new mongoose.Schema( {
+    email:String,
+    password:String
+});
+
+const secret = "Thisisthemylittlesecret";
+userSchema.plugin(encrypt,{secret:secret,encryptedFields: ['password'] });
+
+const User =mongoose.model("User", userSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+app.post("/register", async function (req, res){
+    await User.create({email:req.body.username,password:req.body.password})
+        
+    .catch((err)=>{
+        console.log(err);
+    })
+    .then(()=>{
+        res.render("secrets");
+    });
+});
+
+app.post("/login", async function(req, res){
+    const userName = req.body.username;
+    const password = req.body.password;
+
+    await User.findOne({email:userName}).catch((err)=>{
+        console.log(err);
+    })
+    .then((data)=>{
+        if(data.password===password){
+            res.render("secrets");
+        }else{
+            res.send("Your passwort or username wrong!!")
+        }
+
+    })
+
+
+})
+
+
+
+app.listen(3000,function(){
+    console.log("Server started on port 3000.");
+}) 
